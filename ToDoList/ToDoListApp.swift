5d29d4009af7e5ba6eaeb2bc10b689c577be1dc9
jadrammall <133606195+jadrammall@users.noreply.github.com>
2024-08-23 12:31:34 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Jad Rammal on 23/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
